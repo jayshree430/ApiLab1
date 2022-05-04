cc7dbd87eee3fb9ca3dbd7179628d03fd09d0610
jayshree430 <iyer430@gmail.com>
2022-05-04 20:53:55 +0100

    Created new CoffeeControllerTest class to test new requests. Created tests to check requests with and without Param.

---
+++ src/test/java/com/Jayshree/SpringBoot/ApiLab1/Controller/CoffeeControllerTest.java
@@ -0,0 +1,43 @@
+package com.Jayshree.SpringBoot.ApiLab1.Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class CoffeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvcController;
+
+    @Test
+    public void testGetCoffee() throws Exception{
+        String expectedString = "I like coffee!";
+
+        mockMvcController.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(expectedString));
+    }
+    @Test
+    public void testGetCoffeeWithoutRequestParam() throws Exception {
+        String expectedValue = "latte";
+        this.mockMvcController.perform(MockMvcRequestBuilders.get("/coffee"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(expectedValue));
+    }
+
+    @Test
+    public void testGetCoffeeWithRequestParam() throws Exception{
+        String expectedValue = "cappuccino";
+        this.mockMvcController.perform(MockMvcRequestBuilders.get("/coffee").param("name", "cappuccino"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(expectedValue));
+
+    }
+
+}
