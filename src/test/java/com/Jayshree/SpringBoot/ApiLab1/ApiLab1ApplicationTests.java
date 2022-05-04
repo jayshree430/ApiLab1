fb26586e21ba9f8b8a728d221530e98cd94b15ac
jayshree430 <iyer430@gmail.com>
2022-05-04 20:38:27 +0100

    Initial commit

---
+++ src/test/java/com/Jayshree/SpringBoot/ApiLab1/ApiLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jayshree.SpringBoot.ApiLab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/Jayshree/SpringBoot/ApiLab1/Controller/HomeControllerTest.java
@@ -0,0 +1,4 @@
+package com.Jayshree.SpringBoot.ApiLab1.Controller;
+
+public class HomeControllerTest {
+}
